fd22ad90e216863794c366bcfced806f6bca7122
Dejan Savuljesku <d.savuljesku@gmail.com>
2017-10-20 13:37:57 +0200

    Initial commit
    Translatewiki commit: https://gerrit.wikimedia.org/r/#/c/385360/
    
    Change-Id: Ide8e5ffbb146da5a331206386beb299922a77125
    ERM: #7767

---
+++ resources/bluespice.groupManager.js
@@ -0,0 +1,21 @@
+/**
+ * BlueSpiceGroupManager extension
+ *
+ * @author     Stephan Muggli <muggli@hallowelt.com>
+ * @version    2.22.0
+ * @package    BlueSpiceGroupManager
+ * @copyright  Copyright (C) 2016 Hallo Welt! GmbH, All rights reserved.
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU Public License v2 or later
+ * @filesource
+ */
+Ext.Loader.setPath(
+	'BS.GroupManager',
+	bs.em.paths.get( 'BlueSpiceGroupManager' ) + '/resources/BS.GroupManager'
+);
+ 
+Ext.onReady( function(){
+	Ext.create( 'BS.GroupManager.Panel', {
+		id: 'bs-groupmanager-grid-panel',
+		renderTo: 'bs-groupmanager-grid'
+	} );
+} );
