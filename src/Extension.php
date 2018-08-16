e304606f32a0f61a5dfb0984f0b298f032f20ef1
Peter Boehm <boehm@hallowelt.com>
2018-08-16 09:42:29 +0200

    Refactoring BlueSpiceGroupManager
    -Moved class to Extension.php
    
    Change-Id: Ie1f8c6ce9624e6e573c959768169c1cef2fcfeb5

--- src/Extension.php
+++ src/Extension.php
@@ -1,8 +1,9 @@
 <?php
+
 /**
- * This is the GroupManager class.
+ * GroupManager Extension for BlueSpice
  *
- * The GroupManager offers an easy way to manage the usergroups of the wiki.
+ * Administration interface for adding, editing and deleting usergroups.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,26 +23,24 @@
  *
  * @author     Sebastian Ulbricht <sebastian.ulbricht@dragon-design.hk>
  * @author     Markus Glaser <glaser@hallowelt.com>
- * @package    BlueSpiceGroupManager
- * @copyright  Copyright (C) 2016 Hallo Welt! GmbH, All rights reserved.
+ * @package    BlueSpice_Extensions
+ * @subpackage GroupManager
+ * @copyright  Copyright (C) 2018 Hallo Welt! GmbH, All rights reserved.
  * @license    http://www.gnu.org/copyleft/gpl.html GNU Public License v3
  * @filesource
  */
 
-/**
- * the GroupManager class
- * @package BlueSpiceGroupManager
- */
-class GroupManager extends BsExtensionMW {
+
+namespace BlueSpice\GroupManager;
+
+class Extension extends \BlueSpice\Extension {
 
 	/**
 	 * extension.json callback
 	 * @global array $bsgConfigFiles
 	 */
 	public static function onRegistration() {
-		global $bsgConfigFiles;
-		$bsgConfigFiles['GroupManager']
-			= BSCONFIGDIR . '/gm-settings.php';
+		$GLOBALS[ 'bsgConfigFiles' ][ 'GroupManager' ] = \BSCONFIGDIR . '/gm-settings.php';
 	}
 
 	/**
@@ -49,35 +48,38 @@ class GroupManager extends BsExtensionMW {
 	 * @return array the json answer
 	 */
 	public static function saveData() {
-		global $wgAdditionalGroups, $bsgConfigFiles;
-
-		$sSaveContent = "<?php\n\$GLOBALS['wgAdditionalGroups'] = array();\n\n";
+		global $wgAdditionalGroups;
+		$sSaveContent = "<?php\n\$GLOBALS['wgAdditionalGroups'] = [];\n\n";
 		foreach ( $wgAdditionalGroups as $sGroup => $mValue ) {
-			$aInvalidChars = array();
+			$aInvalidChars = [];
 			$sGroup = trim( $sGroup );
-			if ( substr_count( $sGroup, '\'' ) > 0 ) $aInvalidChars[] = '\'';
-			if ( substr_count( $sGroup, '"' ) > 0 ) $aInvalidChars[] = '"';
+			if ( substr_count( $sGroup, '\'' ) > 0 ) {
+				$aInvalidChars[] = '\'';
+			}
+			if ( substr_count( $sGroup, '"' ) > 0 ) {
+				$aInvalidChars[] = '"';
+			}
 			if ( !empty( $aInvalidChars ) ) {
-				return array(
+				return [
 					'success' => false,
-					'message' => wfMessage( 'bs-groupmanager-invalid-name' )
+					'message' => \wfMessage( 'bs-groupmanager-invalid-name' )
 						->numParams( count( $aInvalidChars ) )
 						->params( implode( ',', $aInvalidChars ) )
 						->text()
-				);
+				];
 			} elseif ( preg_match( "/^[0-9]+$/", $sGroup ) ) {
-				return array(
+				return [
 					'success' => false,
-					'message' => wfMessage( 'bs-groupmanager-invalid-name-numeric' )->plain()
-				);
+					'message' => \wfMessage( 'bs-groupmanager-invalid-name-numeric' )->plain()
+				];
 			} elseif ( strlen( $sGroup ) > 255 ) {
-				return array(
+				return [
 					'success' => false,
-					'message' => wfMessage( 'bs-groupmanager-invalid-name-length' )->plain()
-				);
+					'message' => \wfMessage( 'bs-groupmanager-invalid-name-length' )->plain()
+				];
 			} else {
 				if ( $mValue !== false ) {
-					$sSaveContent .= "\$GLOBALS['wgAdditionalGroups']['{$sGroup}'] = array();\n";
+					$sSaveContent .= "\$GLOBALS['wgAdditionalGroups']['{$sGroup}'] = [];\n";
 					self::checkI18N( $sGroup );
 				} else {
 					self::checkI18N( $sGroup, $mValue );
@@ -87,34 +89,34 @@ class GroupManager extends BsExtensionMW {
 
 		$sSaveContent .= "\n\$GLOBALS['wgGroupPermissions'] = array_merge(\$GLOBALS['wgGroupPermissions'], \$GLOBALS['wgAdditionalGroups']);";
 
-		$res = file_put_contents( $bsgConfigFiles['GroupManager'], $sSaveContent );
+		$res = file_put_contents($GLOBALS[ 'bsgConfigFiles' ][ 'GroupManager' ], $sSaveContent );
 		if ( $res ) {
-			return array(
+			return [
 				'success' => true,
-				'message' => wfMessage( 'bs-groupmanager-grpadded' )->plain()
-			);
+				'message' => \wfMessage( 'bs-groupmanager-grpadded' )->plain()
+			];
 		} else {
-			return array(
+			return [
 				'success' => false,
 				// TODO SU (04.07.11 11:44): i18n
-				'message' => 'Not able to create or write file "' . $bsgConfigFiles['GroupManager'] . '".'
-			);
+				'message' => 'Not able to create or write file "' . $GLOBALS[ 'bsgConfigFiles' ][ 'GroupManager' ] . '".'
+			];
 		}
 	}
 
 	public static function checkI18N( $sGroup, $bValue = true ) {
-		$oTitle   = Title::newFromText( 'group-' . $sGroup, NS_MEDIAWIKI );
+		$oTitle = \Title::newFromText( 'group-' . $sGroup, NS_MEDIAWIKI );
 		$oArticle = null;
 
 		if ( $bValue === false ) {
 			if ( $oTitle->exists() ) {
-				$oArticle = new Article( $oTitle );
+				$oArticle = new \Article( $oTitle );
 				$oArticle->doDeleteArticle( 'Group does no more exist' );
 			}
 		} else {
 			if ( !$oTitle->exists() ) {
-				$oArticle = new Article( $oTitle );
-				$oArticle->doEditContent( ContentHandler::makeContent( $sGroup, $oTitle ), '', EDIT_NEW );
+				$oArticle = new \Article( $oTitle );
+				$oArticle->doEditContent( \ContentHandler::makeContent( $sGroup, $oTitle ), '', EDIT_NEW );
 			}
 		}
 	}
