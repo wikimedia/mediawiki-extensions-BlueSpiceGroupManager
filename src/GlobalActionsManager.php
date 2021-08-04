ee18b9c6dc1ff19f5ae0cc2c07966952cfc69a3f
miriam schlindwein <schlindwein@hallowelt.com>
2021-08-04 14:34:01 +0200

    Add extension to SkinSlot GlobalActionsManager
    [4.x]
    
    Change-Id: Icd299db1143e571aa8855bd3032d38155122a874

---
+++ src/GlobalActionsManager.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace BlueSpice\GroupManager;
+
+use Message;
+use MWStake\MediaWiki\Component\CommonUserInterface\Component\RestrictedTextLink;
+use SpecialPage;
+
+class GlobalActionsManager extends RestrictedTextLink {
+
+	/**
+	 *
+	 */
+	public function __construct() {
+		parent::__construct( [] );
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getId(): string {
+		return 'ga-bs-groupmanager';
+	}
+
+	/**
+	 *
+	 * @return array
+	 */
+	public function getPermissions(): array {
+		$permissions = [
+			'groupmanager-viewspecialpage'
+		];
+		return $permissions;
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getHref(): string {
+		$tool = SpecialPage::getTitleFor( 'GroupManager' );
+		return $tool->getLocalURL();
+	}
+
+	/**
+	 *
+	 * @return Message
+	 */
+	public function getTitle(): Message {
+		return Message::newFromKey( 'bs-groupmanager-desc' );
+	}
+
+	/**
+	 *
+	 * @return Message
+	 */
+	public function getText(): Message {
+		return Message::newFromKey( 'groupmanager' );
+	}
+
+	/**
+	 *
+	 * @return Message
+	 */
+	public function getAriaLabel(): Message {
+		return Message::newFromKey( 'groupmanager' );
+	}
+}
