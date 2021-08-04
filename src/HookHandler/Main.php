0f8185a4270efd180030a799471dacd743de406f
miriam schlindwein <schlindwein@hallowelt.com>
2021-08-04 14:34:01 +0200

    Add extension to SkinSlot GlobalActionsManager
    [4.x]
    
    Change-Id: Icd299db1143e571aa8855bd3032d38155122a874

---
+++ src/HookHandler/Main.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace BlueSpice\GroupManager\HookHandler;
+
+use BlueSpice\GroupManager\GlobalActionsManager;
+use MWStake\MediaWiki\Component\CommonUserInterface\Hook\MWStakeCommonUIRegisterSkinSlotComponents;
+
+class Main implements MWStakeCommonUIRegisterSkinSlotComponents {
+
+	/**
+	 * @inheritDoc
+	 */
+	public function onMWStakeCommonUIRegisterSkinSlotComponents( $registry ): void {
+		$registry->register(
+			'GlobalActionsManager',
+			[
+				'ga-bs-groupmanager' => [
+					'factory' => static function () {
+						return new GlobalActionsManager();
+					}
+				]
+			]
+		);
+	}
+}
