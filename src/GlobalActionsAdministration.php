2c9cfed311d554c463f0884ceb4ad2148867d151
hslater <hamish.slater@hallowelt.com>
2025-01-22 10:37:58 +0100

    Update Message namespace
    ERM38444
    
    [REL1_43, master]

--- src/GlobalActionsAdministration.php
+++ src/GlobalActionsAdministration.php
@@ -2,7 +2,7 @@
 
 namespace BlueSpice\GroupManager;
 
-use Message;
+use MediaWiki\Message\Message;
 use MWStake\MediaWiki\Component\CommonUserInterface\Component\RestrictedTextLink;
 use SpecialPage;
 
